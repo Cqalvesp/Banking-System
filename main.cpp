040ec63c91cbb5b2d5fdd09b10cf1ff9db8fc386
Christian Alves-Patterson <98926428+Cqalvesp@users.noreply.github.com>
2025-10-31 13:32:05 -0400

    greeting message and class modules

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "Welcome to the Christian Bank!" << endl
+         << "1. Make an Account" << endl
+         << "2. Make a deposit" << endl
+         << "3. Make a withdrawal" << endl
+         << "4. Quit" << endl;
+
+    cin >> choice;
+}
