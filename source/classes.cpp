db2b5f5ed2a7d3f1ec2fd4ddb3df6d990e4944e8
Christian Alves-Patterson <98926428+Cqalvesp@users.noreply.github.com>
2025-11-06 18:14:02 -0500

    need to research string formatting lol;also working on rng for account numbers

--- source/classes.cpp
+++ source/classes.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <format>
+#include <cstdlib>
 
 using std::string;
 using namespace std;
 
 // Account class
 class Account {
+private:
     // Private attributes
     int PIN;   
     int LastFourSocial;
+
 public:
     string FirstName;
     string LastName;
@@ -20,7 +23,8 @@ public:
 
     // Constructor
     Account() {
-        cout << "Account object is created.\n";
+        cout << "Account object is created.\n"
+             << "Account Number: ";
     }
 
     // Method to add account attributes to database
@@ -33,6 +37,7 @@ public:
             cout << "Error opening the output file!\n";
         }
 
+        oFile << "\n";
 
         oFile.close();
 
@@ -41,11 +46,35 @@ public:
 
     // Method to increase the balance of account object
     void deposit(double amount) {
+        ofstream oFile;
+
+        auto startingbalance = Balance;
+        Balance += amount;
+
+        oFile.open("~/Desktop/Repos/Banking-System/database/withdrawals.txt");
+        if (!oFile.is_open()) {
+            cout << "Error opening the output file!\n";
+        }
+
+        oFile << "\n";
+
         return;
     }
 
     // Method to increase the balance of account object
     void withdraw(double amount) {
+        ofstream oFile;
+
+        auto startingbalance = Balance;
+        Balance -= amount;
+
+        oFile.open("~/Desktop/Repos/Banking-System/database/withdrawals.txt");
+        if (!oFile.is_open()) {
+            cout << "Error opening the output file!\n";
+        }
+
+        oFile << "\n";
+
         return;
     }
 };
